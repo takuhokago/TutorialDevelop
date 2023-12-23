1feb029fc23778b061e53ee51822af752cb91d3a
takuhokago <takuho1226000@gmail.com>
2023-12-23 18:15:25 +0900

    lesson18 kadai commit

--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -7,6 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,19 +45,24 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("User更新画面")
+    @DisplayName("User一覧画面")
     @WithMockUser
-    void testGetUser() throws Exception {
-        MvcResult result = mockMvc.perform(get("/user/update/1/"))
+    void testGetList() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/list"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attributeExists("userlist"))
                 .andExpect(model().hasNoErrors())
-                .andExpect(view().name("user/update"))
+                .andExpect(view().name("user/list"))
                 .andReturn();
 
-        User user = (User)result.getModelAndView().getModel().get("user");
-        assertEquals(user.getId(), 1);
-        assertEquals(user.getName(), "キラメキ太郎");
+        List<User> userlist = (List<User>)result.getModelAndView().getModel().get("userlist");
+        assertEquals(userlist.size(), 3);
+        assertEquals(userlist.get(0).getName(), "キラメキ太郎");
+        assertEquals(userlist.get(0).getId(), 1);
+        assertEquals(userlist.get(1).getName(), "キラメキ次郎");
+        assertEquals(userlist.get(1).getId(), 2);
+        assertEquals(userlist.get(2).getName(), "キラメキ花子");
+        assertEquals(userlist.get(2).getId(), 3);
     }
 
 
